79a974929bdc2dd87fb415fad9f9c044b3933289
Noel Kwan <noelkwan1998@gmail.com>
2020-02-15 19:46:55 +0800

    Updated layout and intellij scripts

---
+++ intellij.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+export _JAVA_AWT_WM_NONREPARENTING=1
+source idea-community
--- screenlayout.sh
+++ screenlayout.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-1 --off --output DP-1 --mode 1920x1080 --pos 3840x0 --rotate normal --output eDP-1 --primary --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI-2 --mode 1920x1080 --pos 0x0 --rotate normal
+xrandr --output eDP-1 --primary --mode 1280x720 --pos 0x0 --rotate normal --output DP-1 --mode 1920x1080 --pos 1280x0 --rotate normal --output HDMI-1 --off --output HDMI-2 --mode 1280x720 --pos 3200x0 --rotate normal
